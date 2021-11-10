89c997b2651dfa54144c3cd69638f5eda38e71f7
Viraj Jadhav <viraj28j@gmail.com>
2021-11-10 18:06:09 +0530

    t43 api post

--- api-routes/pages/comments/index.js
+++ api-routes/pages/comments/index.js
@@ -2,6 +2,7 @@ import {useState} from 'react'
 
 export default function CommentsPage () {
     const [comments, setComments] = useState([])
+    const [comment, setComment] = useState('')
 
     const fetchComments = async () => {
         const response = await fetch('/api/comments')
@@ -9,9 +10,23 @@ export default function CommentsPage () {
         setComments(data)
     }
 
+    const submitComment = async () => {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({comment}),
+            headers: {
+                'Content-Type': 'application/json'
+            } 
+        })
+        const data = await response.json()
+        console.log(data)
+    }
+
     return (
         <>
         <h1>Comments Page</h1>
+         <input type="text" value={comment} onChange={e => setComment(e.target.value)} />
+            <button onClick={submitComment}>Send me !!!</button>
          <button onClick={fetchComments}>Load Comments</button>
          {
              comments.map(comment => {
